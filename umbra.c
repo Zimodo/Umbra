e66327b8cc113eca3c854783365d8a68c548bbff
Zimodo <zimodo83@gmail.com>
2025-12-02 23:02:46 -0500

    changed pos floats to vectors

--- umbra.c
+++ umbra.c
@@ -5,14 +5,12 @@ const int screenWidth = 800;
 const int screenHeight = 800;
 
 struct Earth{
-    float posX;
-    float posY;
+    Vector2 pos;
     int radius;
 };
 
 struct Moon{
-    float posX;
-    float posY;
+    Vector2 pos;
     float offset;
     float theta;
     int radius;
@@ -31,8 +29,8 @@ int main(){
     struct Moon moon;
 
     // init structs
-    earth.posX = screenWidth/2;
-    earth.posY = screenHeight/2;
+    earth.pos.x = screenWidth/2;
+    earth.pos.y = screenHeight/2;
     earth.radius = 50;
     moon.radius = 20;
     
@@ -73,15 +71,15 @@ void updateEarthAndMoon(struct Earth* earth, struct Moon* moon){
     if (leftStickX > -leftStickDeadzoneX && leftStickX < leftStickDeadzoneX) leftStickX = 0.0f;
     if (leftStickY > -leftStickDeadzoneY && leftStickY < leftStickDeadzoneY) leftStickY = 0.0f; 
 
-    earth->posX += leftStickX*200*GetFrameTime();
-    earth->posY += leftStickY*200*GetFrameTime();
+    earth->pos.x += leftStickX*200*GetFrameTime();
+    earth->pos.y += leftStickY*200*GetFrameTime();
     
     // Screen edge collision
     
-    if (earth->posX > screenWidth - earth->radius) earth->posX = screenWidth - earth->radius;
-    if (earth->posY > screenHeight - earth->radius) earth->posY = screenHeight - earth->radius;
-    if (earth->posX < earth->radius) earth->posX = earth->radius;
-    if (earth->posY < earth->radius) earth->posY = earth->radius;
+    if (earth->pos.x > screenWidth - earth->radius) earth->pos.x = screenWidth - earth->radius;
+    if (earth->pos.y > screenHeight - earth->radius) earth->pos.y = screenHeight - earth->radius;
+    if (earth->pos.x < earth->radius) earth->pos.x = earth->radius;
+    if (earth->pos.y < earth->radius) earth->pos.y = earth->radius;
 
     // Moon
     
@@ -103,8 +101,8 @@ void updateEarthAndMoon(struct Earth* earth, struct Moon* moon){
         }
     }   
     
-    moon->posX = earth->posX + cos(moon->theta)*100;
-    moon->posY = earth->posY + sin(moon->theta)*100;
+    moon->pos.x = earth->pos.x + cos(moon->theta)*100;
+    moon->pos.y = earth->pos.y + sin(moon->theta)*100;
 }
 
 void drawEarthAndMoon(struct Earth* earth, struct Moon* moon){
@@ -116,18 +114,18 @@ void drawEarthAndMoon(struct Earth* earth, struct Moon* moon){
     DrawFPS(10,10);
     
     DrawText(TextFormat("Theta: %f", moon->theta), 200, 80, 20, RED);
-    DrawText(TextFormat("Earth posX: %f", earth->posX), 200, 100, 20, RED);
-    DrawText(TextFormat("Earth posY: %f", earth->posY), 200, 120, 20, RED);
+    DrawText(TextFormat("Earth posX: %f", earth->pos.x), 200, 100, 20, RED);
+    DrawText(TextFormat("Earth posY: %f", earth->pos.y), 200, 120, 20, RED);
 
     // -------
     // Visuals
     // -------
     
     // Draw Earth
-    Vector2 earthPosVector = {earth->posX, earth->posY};
+    Vector2 earthPosVector = {earth->pos.x, earth->pos.y};
     DrawCircleV(earthPosVector, earth->radius, BLUE);
     
     // Draw Moon
-    Vector2 moonPosVector = {moon->posX, moon->posY};
+    Vector2 moonPosVector = {moon->pos.x, moon->pos.y};
     DrawCircleV(moonPosVector, moon->radius, GRAY);
 }
